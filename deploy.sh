75fdb76ebd683f6ccfa02b37577a1f4f1a26dac3
Soare Catalin <stevensuncatalin369@gmail.com>
2024-05-21 14:34:08 +0300

    redeployed the server. also deployed OTP service, starting working with it rn.

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,20 @@ echo "Ready to deploy?"
 
 echo "Stopping the service..."
 
-ssh -i ~/coding/aws/yogabuntu.pem ubuntu@backend.blitzapp.co "sudo service blitz stop"
+ssh -i ~/coding/aws/yogabuntu.pem ubuntu@blitzapp.co "sudo service blitz stop"
 
 echo "Stopped the service!"
 
 echo "Recompilling the code..."
-go build -o backend
+go build -o api
 echo "Recompilled the code!"
 
 echo "Uploading the new binary..."
-scp -i ~/coding/aws/yogabuntu.pem backend ubuntu@backend.blitzapp.co:~/
+scp -i ~/coding/aws/yogabuntu.pem api ubuntu@blitzapp.co:~/
 
 echo "Uploaded the new binary!"
 
 echo "Restarting the service..."
-ssh -i ~/coding/aws/yogabuntu.pem ubuntu@backend.blitzapp.co "sudo service blitz start"
+ssh -i ~/coding/aws/yogabuntu.pem ubuntu@blitzapp.co "sudo service blitz start"
 
 echo "Restarted the service!"
